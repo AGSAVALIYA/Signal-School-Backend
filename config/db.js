f2fdcfe5f603434772b355894fc18a81183d1da8
Akshit Savaliya <akshitsavaliya999@outlook.com>
2023-12-10 13:24:12 +0530

    Fix issue with POSTGRES_URL not defined

--- config/db.js
+++ config/db.js
@@ -3,7 +3,16 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
+
+
 const url = process.env.POSTGRES_URL;
+
+if (!url) {
+  console.error('POSTGRES_URL is not defined in the environment variables.');
+  process.exit(1);
+}
+
+
 const logging = false;
 const dialectOptions = {
   ssl: {
