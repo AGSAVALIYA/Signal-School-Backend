527b1bfb7b1a8fe67e7f8bbd297578208fdd6ef5
Akshit Savaliya <akshitsavaliya999@outlook.com>
2023-12-10 13:06:36 +0530

    Update PORT variable value

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/', (req, res) => {
     res.send('Signal School API');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
