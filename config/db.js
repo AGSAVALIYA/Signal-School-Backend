13d68d7685a5023f848bd7c15be96b07248d05f0
Akshit Savaliya <akshitsavaliya999@outlook.com>
2023-12-13 17:01:08 +0530

    Refactor database configuration

--- config/db.js
+++ config/db.js
@@ -3,36 +3,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
-
-
-// const url = process.env.POSTGRES_URL;
-
-// if (!url) {
-//   console.error('POSTGRES_URL is not defined in the environment variables.');
-//   process.exit(1);
-// }
-
-
-// const logging = false;
-// const dialectOptions = {
-//   ssl: {
-//     require: true
-//   }
-// };
-
-
-// const sequelize = new Sequelize(url, {
-//   logging,
-//   dialectOptions,
-// });
-
-//use this for local development
-const sequelize = new Sequelize('postgres://postgres:password@localhost:5432/signal-school', {
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
   logging: false,
   dialectOptions: {
     ssl: false
   }
 });
+
 
 
 module.exports = sequelize;
