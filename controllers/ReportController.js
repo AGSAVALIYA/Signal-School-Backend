ff2838d24cf98feceaa9d4bfd5d71ea8b1223a54
Akshit Savaliya <akshitsavaliya999@outlook.com>
2023-12-27 08:33:26 +0530

    get Report route and controller added

--- controllers/ReportController.js
+++ controllers/ReportController.js
@@ -1,5 +1,7 @@
 const Report = require("../models/Report");
+const School = require("../models/School");
 const Student = require("../models/Student");
+const Subject = require("../models/Subject");
 
 
 const createReport = async (req, res) => {
@@ -12,8 +14,6 @@ const createReport = async (req, res) => {
     if ( !studentId || !subjectId) {
         throw new Error('Please provide all required fields');
     }
-
-
 
     const student = await Student.findOne({where: {id: studentId}});
     if (!student) {
@@ -35,10 +35,43 @@ const createReport = async (req, res) => {
 }
 
 
+const getCurrentAYReports = async (req, res) => {
+    if (!req.user) {
+        throw new Error('You are not authorized to access this route');
+    }
+    let schoolId;
+    if (req.user === 'admin') {
+        schoolId = req.currentSchool;
+    } else if (req.user === 'teacher') {
+        schoolId = req.teacher.currentSchool;
+    }
+    const studentId = req.params.studentId;
+    const school = await School.findOne({where: {id: schoolId}});
+    const reports = await Report.findAll(
+        {
+            where: {
+                AcademicYearId: school.currentAcademicYear,
+                StudentId: studentId
+            },
+            include: [
+                {
+                    model: Subject,
+                    attributes: ['name']
+                }
+            ]
+        }
+        
+        );
+    // const reports = await Report.findAll({where: {AcademicYearId: school.currentAcademicYear}});
+    return res.status(200).json({message: 'Reports fetched successfully', data: reports});
+}
+
+
 
     
     
     
 module.exports = {
     createReport,
+    getCurrentAYReports
 }
--- routes/report.js
+++ routes/report.js
@@ -5,6 +5,6 @@ const ReportController = require('../controllers/ReportController')
 const tokenVerify = require('../middlewares/tokenVerify')
 
 router.post('/report/create', tokenVerify, ReportController.createReport)
-
+router.get('/report/getForCurrAY/:studentId', tokenVerify, ReportController.getCurrentAYReports)
 
 module.exports = router
